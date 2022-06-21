d60b3a36b883286d9ba6143b9b0c24b7ad4d1410
satwider47 <satwinder47@outlook.com>
2022-06-21 02:21:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+ # Social Network Api [![badge](https://img.shields.io/badge/license-MIT-brightgreen)](./LICENSE)
+
+  ## Description
+
+Social Network Api is a No-Sql app made with mongodb.Where customers can create user accounts and express there thoughts and react on the thoughts of other users.
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+
+  ## Installation
+
+  Packages installed are:<br>  1. npm init <br> 2. npm express <br> 3. npm nodemon <br>4. npm mongoose <br>
+
+  ## Usage
+
+    Social Network app , where users can post tech realted articles. Users can easily mantain their thoughts and reactions. We can create accounts and update our previous thoughts.
+
+  ## License
+
+  [![badge](https://img.shields.io/badge/license-MIT-brightgreen)](./LICENSE)
+This application is covered by the MIT license.
+
+For more information on license types, please reference to this website for additional licensing information - [LICENSE INFORMATION](https://opensource.org/licenses).
+
+  ## Contributing
+
+  Satwinder Singh
+
+  ## Questions?
+
+  Feel free to contact me with example or any question via the information below:
+ * Find me on Github:[satwinder191995](https://github.com/satwinder191995)
+ * Email me with any questions: [satwinder47@outlook.com](mailto:satwinder47@outlook.com)
+
+  ## Video link 
+  [Video link](https://drive.google.com/file/d/1uWUzKuCH211BBs8cVn7PEJ57eXPTcimH/view?usp=sharing)
---
+++ controllers/thought-controller.js
@@ -0,0 +1,128 @@
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+
+    // GET /api/thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    // GET /api/thoughts/:id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => {
+            User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: dbThoughtData._id } },
+                { new: true }
+            )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+
+    // DELETE /api/thoughts/:id
+    deleteThought({ params }, res) {
+        // delete the thought
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            User.findOneAndUpdate(
+                { username: dbThoughtData.username },
+                { $pull: { thoughts: params.id } }
+            )
+            .then(() => {
+                res.json({message: 'Successfully deleted the thought'});
+            })
+            .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    // POST /api/thoughts/:id/reactions
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    deleteReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: body.reactionId } } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json({message: 'Successfully deleted the reactions'});
+        })
+        .catch(err => res.status(500).json(err));
+    },
+}
+
+module.exports = thoughtController;
